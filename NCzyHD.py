623e9cc1d813114f0dd746754561faba98e5c9f7
wusuaya <247483897@qq.com>
2025-10-30 22:13:30 +0800

    Add files via upload

--- NCzyHD.py
+++ NCzyHD.py
@@ -10,7 +10,7 @@ from datetime import datetime
 # ====================================
 API_KEY = "sk-wBuUIEArjm2BoTQBCQgzf2bhzksx87xg3pQ3cPsvccmULhAk"
 BASE_URL = "https://api.sydney-ai.com/v1"
-MODEL_NAME = "gemini-2.5-flash-image-hd"  # 已更新模型名称
+MODEL_NAME = "gemini-2.5-flash-image-preview-vip"  # 已更新模型名称
 
 # ====================================
 # 辅助函数
